44d9031dc8373536b33fd4c35261ec3ae19f860f
HannahJS <41316904+HannahJS@users.noreply.github.com>
2024-07-21 09:29:04 +0100

    Add files via upload

---
+++ day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+float browserVersion=100.2f;
+String browserName="firefox";
+boolean isVisible=true;
+int  releaseYear=1998;
+char  browserLogo='f';
+
+System.out.println("The Browser version is:"+browserVersion);
+System.out.println("The Browser name is :"+browserName);
+System.out.println("The Browser is visible :"+isVisible);
+System.out.println("The Release year is :"+releaseYear);
+System.out.println("The Browser logo is  :"+browserLogo);
+	}
+
+}
